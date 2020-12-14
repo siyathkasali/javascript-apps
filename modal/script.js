c8cc80700b414aafdf511639957216da0ac858c0
Siyath Kasali <jsiyath@gmail.com>
2020-12-14 21:12:18 +0530

    Modal completed

--- modal/script.js
+++ modal/script.js
@@ -1,3 +1,29 @@
 'use strict';
 
-console.log('Hello World!');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const closeModal = document.querySelector('.close-modal');
+const showModal = document.querySelectorAll('.show-modal');
+
+const closeModalFunc = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
+const showModalFunc = () => {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
+
+for(let i = 0; i < showModal.length; i++) {
+  showModal[i].addEventListener('click', showModalFunc)
+}
+
+closeModal.addEventListener('click', closeModalFunc)
+overlay.addEventListener('click', closeModalFunc)
+
+document.addEventListener('keydown', e => {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModalFunc()
+  }
+})
